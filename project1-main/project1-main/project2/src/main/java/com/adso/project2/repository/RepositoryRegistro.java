578ee44f8758979f0a954780d6e5db373c979cce
Miguel Sánchez <129363672+MiguelSanchezlo@users.noreply.github.com>
2024-06-18 10:38:59 -0500

    Mostrar lista

---
+++ project1-main/project1-main/project2/src/main/java/com/adso/project2/repository/RepositoryRegistro.java
@@ -0,0 +1,7 @@
+package com.adso.project2.repository;
+
+import com.adso.project2.model.Registro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositoryRegistro extends JpaRepository<Registro, Long> {
+}
---
+++ project1-main/project1-main/project2/src/main/java/com/adso/project2/service/ServiceRegistro.java
@@ -0,0 +1,22 @@
+package com.adso.project2.service;
+
+import com.adso.project2.model.Registro;
+import com.adso.project2.repository.RepositoryRegistro;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ServiceRegistro {
+    @Autowired
+    private RepositoryRegistro repositoryRegister;
+
+    public List<Registro> getAllRegistro(){
+        return repositoryRegister.findAll();
+    }
+
+    public Registro saveRegister(Registro registro){
+        return repositoryRegister.save(registro);
+    }
+}
