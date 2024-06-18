e41f4344b098af358d66bbb45eb2a877e293a7d6
Miguel Sánchez <129363672+MiguelSanchezlo@users.noreply.github.com>
2024-06-18 10:41:45 -0500

    D

--- project2/src/main/java/com/adso/project2/repository/RepositoryRegistro.java
+++
@@ -1,7 +0,0 @@
-package com.adso.project2.repository;
-
-import com.adso.project2.model.Registro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepositoryRegistro extends JpaRepository<Registro, Long> {
-}
